dfb437887afa9d1d90a57daf8b57f91b03daac1f
Alexey Kotlyarov <a@koterpillar.com>
2016-12-15 16:12:49 +1100

    Day 11

---
+++ puzzle11.hs
@@ -0,0 +1,176 @@
+{-# LANGUAGE TupleSections #-}
+import Control.Applicative
+
+import Data.Char
+import Data.Function
+import Data.List
+import Data.List.Split
+import qualified Data.Map as M
+import Data.Maybe
+import qualified Data.Set as S
+
+
+readLines :: IO [String]
+readLines =
+  getLine >>=
+  \s ->
+     case s of
+       "" -> pure []
+       _ -> fmap (s :) readLines
+
+type Element = String
+
+ewidth :: Int
+ewidth = 2
+
+data Item = Chip Element | Generator Element deriving (Eq)
+
+instance Ord Item where
+  compare = compare `on` itemOrdering
+    where itemOrdering (Chip c) = (c, 2)
+          itemOrdering (Generator c) = (c, 1)
+
+instance Show Item where
+  show (Chip c) = c ++ "M"
+  show (Generator c) = c ++ "G"
+
+type Floor = Int
+
+floors :: [Floor]
+floors = [0 .. 3]
+
+floorLabels :: [String]
+floorLabels = map (\i -> "F" ++ show (i + 1)) floors
+
+data Facility = Facility
+  { fElevator :: Floor
+  , fFloors :: M.Map Item Floor
+  } deriving (Ord, Eq)
+
+instance Show Facility where
+  show (Facility e fs) =
+    unlines $
+    reverse $
+    map (intercalate " ") $
+    transpose $
+    floorLabels :
+    map (uncurry floorDots) (("E" ++ blank', e) : M.toList (M.mapKeys show fs))
+    where
+      floorDots :: String -> Floor -> [String]
+      floorDots s f = replicate f blank ++ [s] ++ replicate (3 - f) blank
+      blank = "." ++ blank'
+      blank' = replicate ewidth ' '
+
+parse :: [String] -> Facility
+parse = Facility 0 . foldr (uncurry parseFloor) M.empty . zip floors
+  where
+    parseFloor :: a -> String -> M.Map Item a -> M.Map Item a
+    parseFloor f s old = foldr (parseItem f) old (tails $ splitOn " " $ filter (not . flip elem ".,") s)
+    parseItem :: a -> [String] -> M.Map Item a -> M.Map Item a
+    parseItem f (elt:"microchip":_) = M.insert (Chip (code elt)) f
+    parseItem f (elt:"generator":_) = M.insert (Generator (code elt)) f
+    parseItem _ _ = id
+    code :: String -> Element
+    code = map toUpper . take ewidth
+
+floorItems :: Floor -> Facility -> [Item]
+floorItems floor (Facility _ is) = [ item | (item, f) <- M.toList is, f == floor]
+
+dangers :: Facility -> [String]
+dangers f = concatMap groupDanger $ map (flip floorItems f) floors
+
+isDangerous :: Facility -> Bool
+isDangerous = not . null . dangers
+
+groupDanger :: [Item] -> [String]
+groupDanger is =
+  let gens =
+        [ elt
+        | Generator elt <- is ]
+      chips =
+        [ elt
+        | Chip elt <- is ]
+      unpowered = filter (not . flip elem gens) chips
+  in if (null gens)
+       then []
+       else map (\elt -> show (Chip elt) ++ " is unpowered") unpowered
+
+data MoveDirection
+  = Up
+  | Down
+  deriving (Ord, Eq, Show)
+
+data Move = Move
+  { mTarget :: Floor
+  , mItems :: [Item]
+  } deriving (Ord, Eq, Show)
+
+moves :: Facility -> [Move]
+moves f@(Facility curFloor _) = Move <$> targets <*> taken
+  where
+    items = floorItems curFloor f
+    targets = filter (flip elem floors) [curFloor + 1, curFloor - 1]
+    taken = filter (flip elem [1, 2] . length) (subsets items)
+
+subsets :: [a] -> [[a]]
+subsets [] = [[]]
+subsets (x:xs) = [id, (x:)] <*> subsets xs
+
+applyUnsafe :: Move -> Facility -> Facility
+applyUnsafe (Move target taken) = moveElevator target . flip (foldr (moveItem target)) taken
+  where
+    moveItem :: Floor -> Item -> Facility -> Facility
+    moveItem target item (Facility e m) = Facility e $ M.insert item target m
+    moveElevator :: Floor -> Facility -> Facility
+    moveElevator target (Facility _ is) = Facility target is
+
+apply :: Move -> Facility -> Maybe Facility
+apply m = checkDanger . applyUnsafe m
+  where
+    checkDanger f
+      | isDangerous f = Nothing
+      | otherwise = Just f
+
+fSuccess :: Facility -> Bool
+fSuccess (Facility _ is) = all (== 3) $ M.elems is
+
+data Tree node edge = Tree
+  { treeNode :: node
+  , treeBranches :: [(edge, Tree node edge)]
+  }
+
+uniqBy :: Ord b => (a -> b) -> [a] -> [a]
+uniqBy key = M.elems . M.fromList . map (\a -> (key a, a))
+
+moveTree :: Ord pos => (pos -> [move]) -> (move -> pos -> Maybe pos) -> pos -> Tree pos move
+moveTree generate apply start = snd $ mtc generate apply start S.empty
+  where
+    mtc
+      :: Ord pos
+      => (pos -> [move])
+      -> (move -> pos -> Maybe pos)
+      -> pos
+      -> S.Set pos
+      -> (S.Set pos, Tree pos move)
+    mtc generate apply start seen = (seen'', Tree start children)
+      where
+        childNodes =
+          uniqBy snd $
+          filter (not . flip S.member seen . snd) $
+          catMaybes
+            [ (move, ) <$> apply move start
+            | move <- generate start ]
+        seen' = S.union seen (S.fromList $ map snd childNodes)
+        (children, seen'') = foldr go ([], seen') childNodes
+        go (move, childNode) (acc, oldSeen) = ((move, result) : acc, newSeen)
+          where
+            (newSeen, result) = mtc generate apply childNode oldSeen
+
+level :: Int -> Tree node edge -> [node]
+level i = map (treeNode . fst) . levelT i
+
+levelT :: Int -> Tree node edge -> [(Tree node edge, [edge])]
+levelT 0 t = [(t, [])]
+levelT i t = do
+  (parent, path) <- levelT (i - 1) t
+  [(child, edge:path) | (edge, child) <- treeBranches parent ]
